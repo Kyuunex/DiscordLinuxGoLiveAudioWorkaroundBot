429681fdc912b154b90cf835edb7dc0e2cea5c49
Kyuunex <kyuunex@protonmail.ch>
2021-03-17 20:54:53 +0400

    add back the ability to attempt to find the bot author again.

--- go-live-bot.py
+++ go-live-bot.py
@@ -17,7 +17,9 @@ else:
 
 owner_list = []
 
-
+intents = discord.Intents.default()
+intents.members = True
+# bot = commands.Bot(command_prefix=command_prefix, intents=intents)
 bot = commands.Bot(command_prefix=command_prefix)
 
 
@@ -51,24 +53,77 @@ async def leave(ctx):
         return
     ctx.voice_client.stop()
     await ctx.voice_client.disconnect()
-    await ctx.message.add_reaction("✅")
+    try:
+        await ctx.message.add_reaction("✅")
+    except:
+        print("channel left")
 
 
 @bot.command()
-async def join(ctx, channel_id):
+async def join(ctx, channel_id=None):
     if not int(ctx.author.id) in owner_list:
         return
 
-    target_channel = bot.get_channel(int(channel_id))
-    if not target_channel:
-        print("can't find a channel with that ID")
-        return
-    await target_channel.connect()
+    target_channel = None
 
-    # ffmpeg -f pulse -i default
-    audio_source = discord.FFmpegPCMAudio("default", before_options="-f pulse", options="")
-    ctx.voice_client.play(audio_source)
-    await ctx.message.add_reaction("✅")
+    if channel_id:
+        target_channel = bot.get_channel(int(channel_id))
+        if not target_channel:
+            print("can't find a channel with that ID")
+            return
+        await target_channel.connect()
+    else:
+        if ctx.guild:
+            if ctx.author.voice:
+                target_channel = ctx.author.voice.channel
+                await target_channel.connect()
+            else:
+                print("You are not connected to a voice channel in this server")
+                return
+        else:
+            if ctx.voice_client is None:
+                member = ctx.author
+                if not member is discord.Member:
+                    for guild in bot.guilds:
+                        member = guild.get_member(ctx.author.id)
+                        if not member:
+                            pass
+                        elif not member.voice:
+                            continue
+                if member:
+                    if member.voice:
+                        target_channel = member.voice.channel
+                        await target_channel.connect()
+                    else:
+                        print("You are not connected to a voice channel.")
+                        print("or something is broken. "
+                              "maybe try specifying a channel ID if you insist on using the bot though DMs")
+                        return
+                else:
+                    print("Using this bot through DMs without specifying a channel ID, "
+                          "requires you enable SERVER MEMBERS INTENT over at "
+                          f" https://discord.com/developers/applications/{bot.user.id}/bot")
+                    print("then you need to edit this script, uncomment line 22 and comment 23.")
+                    print("if you are seeing this message, you haven't did that")
+                    return
+            elif ctx.voice_client.is_playing():
+                ctx.voice_client.stop()
+            else:
+                return
+
+    if not target_channel:
+        print("I am not able to find a channel to join in")
+        return
+
+    for voice_client in bot.voice_clients:
+        if voice_client.channel == target_channel:
+            # ffmpeg -f pulse -i default
+            audio_source = discord.FFmpegPCMAudio("default", before_options="-f pulse", options="")
+            voice_client.play(audio_source)
+            try:
+                await ctx.message.add_reaction("✅")
+            except:
+                print(f"joined {target_channel.name}")
 
 
 bot.run(bot_token)
